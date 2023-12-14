ad24c15330a911adaad47111dc758656c6b0010a
japheth <japhemchirie@gmail.com>
2023-12-14 19:16:44 +0300

    override takenRefreshView class to set cookie on refresh token

--- users/views.py
+++ users/views.py
@@ -34,3 +34,28 @@ class CustomerTokenObtainPairView(TokenObtainPairView):
             )
 
         return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh')
+
+        if refresh_token:
+            request.data['refresh'] = refresh_token
+
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            access_token = response.data.get('access')
+
+            response.set_cookie(
+                'access',
+                access_token,
+                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAME_SITE,
+            )
+
+        return response
